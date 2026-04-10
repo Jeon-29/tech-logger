c32beb0b3e32b96e6ac1e423c8f74a48787b529f
Jeon <masibayanjelo8@gmail.com>
2026-04-10 15:12:34 +0800

    feat: implement store logic to save repair logs to data

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Device;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        Device::create([
+            'client_name' =>$request->client_name,
+            'device_model' =>$request->device_model,
+            'issue_description' =>$request->issue_description,
+            'status' => 'pending',
+        ]);
+    }
 }
