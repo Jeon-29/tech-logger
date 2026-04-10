168f64fcad706b7c3be76c514f0c3da31a0f6660
Jeon <masibayanjelo8@gmail.com>
2026-04-10 18:04:41 +0800

    added confirmed pass validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -14,7 +14,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|confirmed|min:8',
+            'password' => 'required|confirmed|min:8',
         ]);
 
         $user = User::Create([
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,21 +3,25 @@
 @section('title', 'Register')
 
 @section('content')
-    <h3 class="text-white text-center mb-4">Create Account</h3>
-    <form action="/register" method="POST">
-        @csrf
-        <div class="mb-3">
-            <input type="text" name="name" class="form-control" placeholder="Name" required>
-        </div>
-        <div class="mb-3">
-            <input type="email" name="email" class="form-control" placeholder="Email Address" required>
-        </div>
-        <div class="mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password" required>
-        </div>
-        <button type="submit" class="btn btn-light w-100 fw-bold py-3 rounded-4 shadow-sm">Create Account</button>
-    </form>
-    <div class="text-center mt-4">
-        <a href="/login" class="text-white text-decoration-none small opacity-75">Already have an account? Login</a>
+<h3 class="text-white text-center mb-4">Create Account</h3>
+<form action="/register" method="POST">
+    @csrf
+    <div class="mb-3">
+        <input type="text" name="name" class="form-control" placeholder="Name" required>
     </div>
+    <div class="mb-3">
+        <input type="email" name="email" class="form-control" placeholder="Email Address" required>
+    </div>
+    <div class="mb-3">
+        <input type="password" name="password" class="form-control" placeholder="Create Password" required>
+    </div>
+
+    <div class="mb-3">
+        <input type="password" name="password_confirmation" class="form-control" placeholder="Confirm Password" required>
+    </div>
+    <button type="submit" class="btn btn-light w-100 fw-bold py-3 rounded-4 shadow-sm">Create Account</button>
+</form>
+<div class="text-center mt-4">
+    <a href="/login" class="text-white text-decoration-none small opacity-75">Already have an account? Login</a>
+</div>
 @endsection
